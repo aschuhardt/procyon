f1450f8a7a4e3e7ae55f818182be5de372b1bdf9
Addison Schuhardt <addison@schuhardt.net>
2020-08-15 13:32:06 -0700

    adding a close_window function in order to expose that capability to bindings

--- src/window.c
+++ src/window.c
@@ -300,3 +300,4 @@ void procy_begin_loop(window_t* window) {
 
 void procy_set_clear_color(color_t c) { glClearColor(c.r, c.g, c.b, 1.0F); }
 
+void procy_close_window(procy_window_t* window) { window->quitting = true; }
