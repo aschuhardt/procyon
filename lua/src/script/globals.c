5bbcc8ff0b8f83505c183a4e7e3ba5ca9650e7f5
Addison Schuhardt <addison@schuhardt.net>
2021-11-19 21:49:37 -0800

    using _fullpath for mingw builds instead of realpath

--- lua/src/script/globals.c
+++ lua/src/script/globals.c
@@ -15,7 +15,12 @@ void add_globals(lua_State *L, script_env_t *env, const char *path) {
   lua_pushlightuserdata(L, env->window);
   lua_setfield(L, LUA_REGISTRYINDEX, GLOBAL_WINDOW_PTR);
 
+#ifdef __MINGW32__
+  char *absolute = _fullpath(path, NULL, PATH_MAX);
+#else
   char *absolute = realpath(path, NULL);
+#endif
+
   if (absolute == NULL) {
     log_error("Failed to obtain the absolute path of the script entry file");
     return;
