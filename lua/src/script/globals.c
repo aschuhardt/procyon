c28b2371525482590da3f6f37b2282b0af869a89
Addison Schuhardt <aschuhardt@users.noreply.github.com>
2021-12-01 11:31:45 -0800

    troubleshooting a heap corruption error in MinGW builds

--- lua/src/script/globals.c
+++ lua/src/script/globals.c
@@ -2,9 +2,15 @@
 #include <log.h>
 #include <lua.h>
 #include <stdlib.h>
+#include <string.h>
+#include <limits.h>
 
 #include "script.h"
 #include "script/environment.h"
+
+#ifndef PATH_MAX
+  #define PATH_MAX 1 << 16
+#endif
 
 #define FIELD_ROOT "ROOT"
 
@@ -15,21 +21,19 @@ void add_globals(lua_State *L, script_env_t *env, const char *path) {
   lua_pushlightuserdata(L, env->window);
   lua_setfield(L, LUA_REGISTRYINDEX, GLOBAL_WINDOW_PTR);
 
+  char abs_path[PATH_MAX] = {0};
+  
 #ifdef __MINGW32__
-  char *absolute = _fullpath(path, NULL, PATH_MAX);
+  _fullpath(path, &abs_path[0], sizeof(abs_path));
 #else
-  char *absolute = realpath(path, NULL);
+  realpath(path, &abs_path[0]);
 #endif
 
-  if (absolute == NULL) {
-    log_error("Failed to obtain the absolute path of the script entry file");
-    return;
+  if (strlen(&abs_path[0]) > 0) {
+    const char *root = dirname(&abs_path[0]);
+    lua_pushstring(L, root);
+    lua_setglobal(L, FIELD_ROOT);
+  } else {
+    log_warn("Unable to determine the root script directory, so ROOT will be undefined");
   }
-
-  const char *root = dirname(absolute);
-
-  lua_pushstring(L, root);
-  lua_setglobal(L, FIELD_ROOT);
-
-  free(absolute);
 }
